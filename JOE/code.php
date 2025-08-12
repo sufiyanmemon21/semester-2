989a653d0604601ff7cbde3fa353417f61cf02ca
sufiyanmemon21 <sufiyanmemon393@gmail.com>
2025-08-12 13:02:03 +0500

    Add files via upload

---
+++ JOE/code.php
@@ -0,0 +1,12 @@
+<?php
+
+include "connection.php";
+
+if (isset($_POST['register'])){
+    
+    $name= $_POST ['name'];
+    $name= $_POST ['email'];
+    $name= $_POST ['pass'];
+}
+
+?>
---
+++ JOE/connection.php
@@ -0,0 +1,9 @@
+<?php
+
+$con= mysqli_connect("localhost","root","","form_handling");
+
+if($con){
+    echo "db connected";
+}
+
+?>
